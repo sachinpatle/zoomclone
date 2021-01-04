0612289989e6b5a3eabb8c556af790adf54ddf51
sachinpatle <sachinpatle189@gmail.com>
2021-01-04 13:29:47 +0530

    second version

--- public/script.js
+++ public/script.js
@@ -68,4 +68,62 @@ if (e.which  == 13  && text.val().length  !== 0){
 })
 socket.on('createmessage',message=>{
     $('ul').append(`<li class="message"><b>User</b><br/>${message}</li>`)
+    socketToBottom()
 })
+
+const socketToBottom=()=>{
+    let d =$('.main__chat_window');
+    d.scrollTop(d.prop('scrollHeight'));
+}
+
+const muteUnmute = ()=>{
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if(enabled){
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton();
+
+    }
+    else{
+        setMuteButton();
+        myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+}
+
+const setMuteButton =()=>{
+    const html=`<i class="fas fa-microphone"></i>
+    <span>mute</span>`
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton =()=>{
+    const html=`<i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>`
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const playStop=()=>{
+    let enabled =myVideoStream.getVideoTracks()[0].enabled;
+    if(enabled){
+        myVideoStream.getVideoTracks()[0].enabled = false;
+        setPlayVideo()
+    }
+    else
+    {
+        setStopVideo()
+        myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+}
+const setStopVideo = ()=>{
+    const html = `<i class="fas fa-video"></i>
+    <span>Stop Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const setPlayVideo = ()=>{
+    const html = `<i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
+}
+
